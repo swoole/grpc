3f014e890de961154dffbb255b3168db69c74b8d
twosee <twose@qq.com>
2018-08-16 18:52:14 +0800

    Improve the stress example.

--- examples/etcd/stress.php
+++ examples/etcd/stress.php
@@ -4,7 +4,6 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 
 ini_set('memory_limit', '154M');
 co::set(['max_coroutine' => 10002]);
-$start = microtime(true);
 go(function () {
     $kvClient = new Etcdserverpb\KVClient('macos:2379');
     $kvClient->start();
@@ -12,6 +11,7 @@ go(function () {
     $request->setPrevKv(true);
     $request->setValue('Swoole');
 
+    $start = microtime(true);
     for ($i = 10000; $i--;) {
         go(function () use ($kvClient, $request, $i) {
             $request->setKey("Hello{$i}");
@@ -21,15 +21,12 @@ go(function () {
                 echo "Error#{$status}: {$reply}\n";
                 exit;
             }
-            if ($i === 0) {
-                global $start;
-                echo 'use time: ' . (microtime(true) - $start) . "s\n";
-                var_dump($kvClient->stats());
-                var_dump(memory_get_usage(true));
-            }
         });
     }
 
     // wait all of the responses back
     $kvClient->closeWait();
+    echo 'use time: ' . (microtime(true) - $start) . "s\n";
+    var_dump($kvClient->stats());
+    var_dump(memory_get_usage(true));
 });

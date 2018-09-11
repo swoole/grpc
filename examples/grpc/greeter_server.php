bb19279e7d0a4ee18cbc96644dcf2048ef9215da
twosee <twose@qq.com>
2018-09-11 16:49:45 +0800

    Improve greeter examples.

---
+++ examples/grpc/greeter_server.php
@@ -0,0 +1,38 @@
+<?php
+use Helloworld\HelloReply;
+use Helloworld\HelloRequest;
+
+require __DIR__ . '/../../../vendor/autoload.php';
+require __DIR__ . '/../../include/functions.php';
+
+$http = new swoole_http_server('0.0.0.0', 50051, SWOOLE_BASE);
+$http->set([
+    'log_level' => SWOOLE_LOG_INFO,
+    'trace_flags' => 0,
+    'worker_num' => 1,
+    'open_http2_protocol' => true
+]);
+$http->on('workerStart', function (swoole_http_server $server) {
+    echo "nghttp -v http://127.0.0.1:{$server->port}\n";
+});
+$http->on('request', function (swoole_http_request $request, swoole_http_response $response) use ($http) {
+    /**@var $request_message HelloRequest */
+    $request_message = Grpc\Parser::deserializeMessage([HelloRequest::class, null], $request->rawcontent());
+    if ($request_message) {
+        $response_message = new HelloReply();
+        $response_message->setMessage('Hello ' . $request_message->getName());
+        $response->header('content-type', 'application/grpc');
+        $response->header('trailer','grpc-status, grpc-message');
+        $headers = [
+            "grpc-status" => "0",
+            "grpc-message" => ""
+        ];
+        foreach ($headers as $header_name => $header_value) {
+            $response->trailer($header_name, $header_value);
+        }
+        $response->end(Grpc\Parser::serializeMessage($response_message));
+    } else {
+        $response->end('failed');
+    }
+});
+$http->start();

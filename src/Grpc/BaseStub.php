3474407630006af3ac8f5494c501a88c863023c2
twosee <twose@qq.com>
2020-06-01 16:24:12 +0800

    Rename

--- src/Grpc/BaseStub.php
+++ src/Grpc/BaseStub.php
@@ -1,7 +1,7 @@
 <?php
 /*
   +----------------------------------------------------------------------+
-  | Swoole-Etcd-Client                                                   |
+  | Swoole-gRPC                                                   |
   +----------------------------------------------------------------------+
   | This source file is subject to version 2.0 of the Apache license,    |
   | that is bundled with this package in the file LICENSE, and is        |
